99bb6c24d3fe66cb2e0d21f23d17faca446efcf5
Yasin <146708314+cheeseburgerhere@users.noreply.github.com>
2023-11-16 21:14:49 +0300

    Add files via upload

---
+++ UnitTest.py
@@ -0,0 +1,16 @@
+from main import scrape_news
+import unittest as test
+
+class TestClass(test.TestCase):
+    def test_null(self):
+        with self.assertRaises(TypeError):
+            scrape_news(time_range="10 days ago",selected_topics="",num_articles=2)
+    def test_invalidNumber(self):    
+        with self.assertRaises(IndexError):
+            scrape_news(time_range="10 days ago",selected_topics="Technology news",num_articles=-1)
+    def test_type(self):
+        with self.assertRaises(TypeError):
+            scrape_news(time_range="10 days ago",selected_topics=7,num_articles=2)
+    def test_date(self):
+        with self.assertRaises(TypeError):
+            scrape_news(time_range="why",selected_topics="Technology news",num_articles=2)
