d96ed7635980620201da400b4ef463572dfec4b1
Yasin <146708314+cheeseburgerhere@users.noreply.github.com>
2023-12-31 13:07:16 +0300

    Add files via upload

--- SingleSiteContents/singleSiteVer.py
+++ SingleSiteContents/singleSiteVer.py
@@ -1,11 +1,13 @@
 from dotenv import load_dotenv
+import pandas as pd
+
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import AsyncChromiumLoader
 from langchain.document_transformers import BeautifulSoupTransformer
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains import create_extraction_chain
-import pandas as pd
-import datetime
+
+from datetime import datetime
 from dateutil import parser
 from dateutil.relativedelta import relativedelta
 from langchain.schema.document import Document
@@ -15,13 +17,13 @@ load_dotenv()
 llm=ChatOpenAI(temperature=0)
 
 #region helpfunctions
-def __webPull(link:list,tags:list, split:bool=True): #pulls data fram website
+def __webPull(link:list,tags:list, split:bool=True, unwanted:list=[]): #pulls data fram website
     loader=AsyncChromiumLoader(link)
     docs=loader.load()
 
     bs_transformer=BeautifulSoupTransformer()
     docs_transformed=bs_transformer.transform_documents(
-        docs,tags_to_extract=tags
+        docs,tags_to_extract=tags,unwanted_tags=unwanted
     )
     if split:
         splitter=RecursiveCharacterTextSplitter.from_tiktoken_encoder(chunk_size=1000, chunk_overlap=0)
@@ -36,7 +38,7 @@ def __extract(content:str,schema:dict,llm:ChatOpenAI):
 
 def __parseDate(date_string:str): #pareses the dates to the correct form
     try:
-        current_datetime = datetime.datetime.now()
+        current_datetime = datetime.now()
         tempList=date_string.split(" ")
         if(tempList[0]=="Published"):
             sum=""
@@ -57,7 +59,7 @@ def __parseDate(date_string:str): #pareses the dates to the correct form
         return None
 
 
-def __list_to_df(input:dict, sites : dict, reqDate:datetime.datetime, num): #creates the dataframe
+def __list_to_df(input:dict, sites : dict, reqDate:datetime, num): #creates the dataframe
     names=[]
     dates=[]
     siteN=[]
@@ -127,12 +129,16 @@ def linkSplitter(links:str):
     return linksDic
 
 def contentsPuller(df:pd.DataFrame): #gets the article content
-    siteLinks:list=df.iloc[:,2]
+    siteLinks:list=df.iloc[:,2].to_list()
 
-    pull=__webPull(link=siteLinks,tags=["div","p"], split=False)
+    pull=__webPull(link=siteLinks,tags=["div","p"], split=False, unwanted=["h3","span","a","img","li","ul"])
     content = []
     for c in pull:
         content.append(inputSplitter(c.page_content))
+        #sometimes script can't acces the website so if that happens we need to try again
+        #when that error happens BeautifulSoup will give an error but there should be no problem in the output
+        if(len(c.page_content)<2):
+            return contentsPuller(df)
 
     df:pd.Series=pd.Series(content)
     df.name="Contents"
@@ -162,12 +168,13 @@ def scrape_news(time_range : str, selected_topics : str, num_articles : int):
         },
         "required":["article_title","article_date"]
     }
-    while(len(endDf)<num_articles and i<6):
+    while(len(endDf)<num_articles and i<11):
         bbc="https://www.bbc.co.uk/search?q="+selected_topics+"&d=NEWS_GNL&seqId=6dacb860-94ed-11ee-bf63-d95cb16fc5af&page="+str(i)
 
         links = [bbc]
         splits = __webPull(link=links,tags=["h3","div","span"],split=True)
         linksPuller : Document=__webPull(link=links,tags=["a"],split=True)
+
         linksDic: dict = linkSplitter(linksPuller[0].page_content[1091:])
         #print(linksDic)
         content = inputSplitter(splits[0].page_content)
@@ -185,8 +192,8 @@ def scrape_news(time_range : str, selected_topics : str, num_articles : int):
 
     articleContent=contentsPuller(endDf)
     endDf=pd.merge(df,articleContent,right_index=True,left_index=True)
-    #df.to_csv("val3.csv")
-    #print(endDf)
+    
+    print(endDf)
     return endDf
 
-scrape_news(time_range="10 days ago",selected_topics="world",num_articles=2)
+scrape_news(time_range="40 days ago",selected_topics="war",num_articles=3)
